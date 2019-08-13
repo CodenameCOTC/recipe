15ec5d2d1c15b50df4a5b3808673b6663760a69f
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-13 13:38:00 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DeliMeals'),
+        ),
+        body: Center(
+          child: Text('Navigation time!'),
+        ),
+      ),
+    );
+  }
+}
