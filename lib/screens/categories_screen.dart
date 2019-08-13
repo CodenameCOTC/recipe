721f637940c1af629b194e415c17d4a15232c9b1
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-13 21:30:30 +0700

    add bottom tabs

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,23 +7,20 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print(context);
-    return Scaffold(
-      appBar: AppBar(title: Text('Delimeals')),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map((category) => CategoryItem(
-                  category.id,
-                  category.title,
-                  category.color,
-                ))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(
+                category.id,
+                category.title,
+                category.color,
+              ))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
